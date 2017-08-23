aefa287fb0502fdb5304e659eaa0cf48ecd20744
nbasnet <nbasnet@outlook.com>
2017-08-22 23:05:54 -0700

    basic setup of validation library

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import com.nischal.services.file.xml.XmlParser
+import com.nischal.services.validation._
 import models.{Catalog, ItemSize}
 
 import scala.xml._
@@ -13,14 +14,56 @@ import scala.xml._
 object Main extends App
 {
 
-  val xmlFile = this.getClass.getResource("product-test.xml")
-  val xmlSource = XML.load(xmlFile.getPath)
+  xmlTest()
+  validationTest()
 
-  val catalog = XmlParser.parse(xmlSource)(Catalog.xmlRead)
+  def xmlTest(): Unit =
+  {
+    val xmlFile = this.getClass.getResource("product-test.xml")
+    val xmlSource = XML.load(xmlFile.getPath)
 
-  catalog.foreach(println)
+    val catalog = XmlParser.parse(xmlSource)(Catalog.xmlRead)
 
-  val newImg = XmlParser.parse(xmlSource)(ItemSize.xmlRead)
+    catalog.foreach(println)
 
-  newImg.foreach(println)
+    val newImg = XmlParser.parse(xmlSource)(ItemSize.xmlRead)
+
+    newImg.foreach(println)
+  }
+
+  def validationTest(): Unit =
+  {
+    val usr = User("bat", "mat", 2)
+    println("==============FIRST USER VALIDATION==============")
+    usr.validate().foreach(println)
+
+    val usr2 = User("usr_idds", "haa", 10, Some("homerr"))
+    println("==============SECOND USER VALIDATION==============")
+    usr2.validate().foreach(println)
+  }
+}
+
+case class User(
+  user_id: String,
+  first_name: String,
+  age: Int,
+  group: Option[String] = None
+)
+{
+
+  import com.nischal.services.validation.DefaultValidations._
+
+  private val validationsNew: Seq[IFieldValidator[_]] = Seq(
+    StringField("user_id", user_id,
+      StartsWith("usr") and Length(min = 5)
+    ),
+    IntField("age", age,
+      InBetween(min = 2)
+    ),
+    OptField[String]("group", group,
+      StartsWith("aa") and Length(max = 4, min = 2)
+    )
+  )
+
+  def validate(group: Option[String] = None): Seq[String] = validationsNew.flatMap(_.validate(group))
 }
